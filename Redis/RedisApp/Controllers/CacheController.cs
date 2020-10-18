0c1998cc723c637c840427797390a81635b23ca7
noble-hub <noblenoble097@gmail.com>
2020-10-18 18:40:21 +0530

    Redis Cache Application connect to Redis container and set/get values. 1. docker pull redis 2. docker run --name local-redis -p 6379:6379 -d redis 3. docker logs -f local-redis 4. docker exec -it local-redis /bin/bash 5. redis-cli 6. ping (Get response pong) 7. set mykey Test 8. get mykey If the application is running in IIS Express then we can use localhost for connecting ConnectionMultiplexer. If we are running in docker the need to find the docker IPv4 address using ipconfig command and need to provide that IP (172.28.176.1).

---
+++ Redis/RedisApp/Controllers/CacheController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+using StackExchange.Redis;
+
+namespace RedisApp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CacheController : ControllerBase
+    {
+        private readonly IDatabase database;
+
+        public CacheController(IDatabase database)
+        {
+            this.database = database;
+        }
+
+        [HttpGet]
+        public string Get([FromQuery] string key)
+        {
+            return database.StringGet(key);
+        }
+
+        // POST: api/Cache
+        [HttpPost]
+        public void Post([FromBody] KeyValuePair<string, string> keyValue)
+        {
+            database.StringSet(keyValue.Key, keyValue.Value);
+        }
+    }
+}
